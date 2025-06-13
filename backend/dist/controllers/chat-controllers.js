c9b8f88ceeadd352986cc2b56d6058f459fffcdb
vishnukantshukla <vishnukantshukla820@gmail.com>
2025-06-13 19:56:30 +0530

    Updated files

--- backend/dist/controllers/chat-controllers.js
+++ backend/dist/controllers/chat-controllers.js
@@ -9,17 +9,35 @@ const openai_config_1 = require("../config/openai-config");
 const openai_1 = __importDefault(require("openai"));
 const generateChatCompletion = async (req, res, next) => {
     const { message } = req.body;
-    const user = await User_1.default.findById(res.locals.jwtData.id);
-    if (!user)
-        return res.status(401).json({ message: "User not registered Or Token malfunctioned" });
-    // grab chats of the user
-    const chats = user.chats.map(({ role, content }) => ({ role, content }));
-    chats.push({ content: message, role: "user" });
-    user.chats.push({ content: message, role: "user" });
-    // sent all chats with new one to openapi API
-    const config = (0, openai_config_1.configureOpenAI)();
-    const openai = new openai_1.default(config);
-    // get latest response
+    try {
+        const user = await User_1.default.findById(res.locals.jwtData.id);
+        if (!user)
+            return res
+                .status(401)
+                .json({ message: "User not registered Or Token malfunctioned" });
+        // grab chats of the user
+        const chats = user.chats.map(({ role, content }) => ({
+            role,
+            content,
+        }));
+        chats.push({ content: message, role: "user" });
+        user.chats.push({ content: message, role: "user" });
+        // sent all chats with new one to openapi API
+        const config = (0, openai_config_1.configureOpenAI)();
+        const openai = new openai_1.default(config);
+        // get latest response
+        const chatResponse = await openai.createChatCompletion({
+            model: "gpt-3.5-turbo", // or your desired model
+            messages: chats,
+        });
+        user.chats.push(chatResponse.data.choices[0].message);
+        await user.save();
+        return res.status(200).json({ chats: user.chats });
+    }
+    catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: "Something Went Wrong" });
+    }
 };
 exports.generateChatCompletion = generateChatCompletion;
 //# sourceMappingURL=chat-controllers.js.map
