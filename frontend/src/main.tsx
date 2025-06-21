44635ef693739694a61af40a4275b4f48b3a11d5
vishnukantshukla <vishnukantshukla820@gmail.com>
2025-06-21 23:58:15 +0530

    Updated Files

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -8,7 +8,7 @@ import { AuthProvider } from './context/AuthContext.tsx'
 import {Toaster} from 'react-hot-toast';
 import axios from "axios"
 
-axios.defaults.baseURL =  "http://localhost:5000/api/v1";
+axios.defaults.baseURL =  "https://ai-chatbot-backends.onrender.com";
 
 axios.defaults.withCredentials = true; // it basically helpps us directly exchange the cookies from the backend
 const theme = createTheme({typography:{fontFamily:"Roboto Slab,serif",allVariants:{color:"white"}},});
