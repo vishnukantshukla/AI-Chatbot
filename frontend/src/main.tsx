977da1e60c2f714805646ee3e29117dcb7a62eff
vishnukantshukla <vishnukantshukla820@gmail.com>
2025-06-21 12:47:18 +0530

    updated files

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -8,7 +8,7 @@ import { AuthProvider } from './context/AuthContext.tsx'
 import {Toaster} from 'react-hot-toast';
 import axios from "axios"
 
-axios.defaults.baseURL =  "http://localhost:5000/api/v1";
+axios.defaults.baseURL =  "http://localhost:10000/api/v1";
 axios.defaults.withCredentials = true; // it basically helpps us directly exchange the cookies from the backend
 const theme = createTheme({typography:{fontFamily:"Roboto Slab,serif",allVariants:{color:"white"}},});
 
