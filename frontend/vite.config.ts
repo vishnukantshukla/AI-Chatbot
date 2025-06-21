f500873788c1bc21c6abd0752a6f6291b4aeca10
vishnukantshukla <vishnukantshukla820@gmail.com>
2025-06-22 01:00:25 +0530

    Updated Files

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: true,
+    port: process.env.PORT ? parseInt(process.env.PORT) : 5173,
+  },
+});
